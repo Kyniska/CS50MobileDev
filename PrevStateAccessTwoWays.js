6b3cf157e4e687469f3f6686cd318e2e70f2c3a7
Kyniska <Kyniska@users.noreply.github.com>
2019-05-14 13:19:47 +0200

    Update PrevStateAccessTwoWays.js

--- PrevStateAccessTwoWays.js
+++ PrevStateAccessTwoWays.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-class Player extends React.Component {
+class Player extends Component {
     constructor() {
         super()
         this.state = { score: 0 }
     }
     increaseScore() {
         // get previous state from tihs.state
-        this.setState({ score: this.state.score + 1 })
+        // this.setState({ score: this.state.score + 1 })
 
         // get previous state from callback function
         this.setState(prevState => ({ 
             score: prevState.score + 1 }))
     }
     return() {
-        <Text>{this.state.score}</Text>
+        <Text>{() => this.increaseScore()}</Text>
+        // have also tried <Text>{this.state.score}</Text>
     }
 }
 
-export default class App extends React.Component {
+export default class App extends Component {
     render() {
         return (
             <View>
