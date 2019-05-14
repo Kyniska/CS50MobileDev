aabce7008e27eef00724f8a30eb8854a720f31e7
Kyniska <Kyniska@users.noreply.github.com>
2019-05-14 13:17:10 +0200

    Add files via upload

---
+++ PrevStateAccessTwoWays.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Player extends React.Component {
+    constructor() {
+        super()
+        this.state = { score: 0 }
+    }
+    increaseScore() {
+        // get previous state from tihs.state
+        this.setState({ score: this.state.score + 1 })
+
+        // get previous state from callback function
+        this.setState(prevState => ({ 
+            score: prevState.score + 1 }))
+    }
+    return() {
+        <Text>{this.state.score}</Text>
+    }
+}
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Player />
+            </View>
+        )
+    }
+}
